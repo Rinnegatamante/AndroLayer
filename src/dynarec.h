4743ac28e02b9128454765c6b4992a955bc45be3
Rinnegatamante <rinnegatamante@gmail.com>
2024-10-05 15:51:05 +0200

    Simplify dynarec config.

--- src/dynarec.h
+++ src/dynarec.h
@@ -14,83 +14,94 @@
 class so_env final : public Dynarmic::A64::UserCallbacks {
 public:
 	std::uint64_t ticks_left = 0;
-	std::uint64_t total_ticks = 0;
 	std::uint8_t *memory = nullptr;
 	std::uint64_t mem_size = 0;
 
 	std::uint64_t getCyclesForInstruction(bool isThumb, std::uint32_t instruction) {
-        (void)isThumb;
-        (void)instruction;
+		(void)isThumb;
+		(void)instruction;
 
-        return 1;
-    }
+		return 1;
+	}
 
 	std::uint8_t MemoryRead8(std::uint64_t vaddr) override {
 		return memory[vaddr];
 	}
 
 	std::uint16_t MemoryRead16(std::uint64_t vaddr) override {
-		return std::uint16_t(MemoryRead8(vaddr)) | std::uint16_t(MemoryRead8(vaddr + 1)) << 8;
+		return *(std::uint16_t *)(memory + vaddr);
 	}
 
 	std::uint32_t MemoryRead32(std::uint64_t vaddr) override {
-		return std::uint32_t(MemoryRead16(vaddr)) | std::uint32_t(MemoryRead16(vaddr + 2)) << 16;
+		return *(std::uint32_t *)(memory + vaddr);
 	}
 
 	std::uint64_t MemoryRead64(std::uint64_t vaddr) override {
-		return std::uint64_t(MemoryRead32(vaddr)) | std::uint64_t(MemoryRead32(vaddr + 4)) << 32;
+		return *(std::uint64_t *)(memory + vaddr);
 	}
 	
 	Dynarmic::A64::Vector MemoryRead128(std::uint64_t vaddr) override {
 		Dynarmic::A64::Vector data;
-		data[0] = MemoryRead64(vaddr);
-		data[1] = MemoryRead64(vaddr + 8);
+		data[0] = *(std::uint16_t *)(memory + vaddr);
+		data[1] = *(std::uint16_t *)(memory + vaddr + 8);
 		return data;
 	}
 
 	void MemoryWrite8(std::uint64_t vaddr, std::uint8_t value) override {
-		if (vaddr >= mem_size) {
-			return;
-		}
 		memory[vaddr] = value;
 	}
 
 	void MemoryWrite16(std::uint64_t vaddr, std::uint16_t value) override {
-		MemoryWrite8(vaddr, std::uint8_t(value));
-		MemoryWrite8(vaddr + 1, std::uint8_t(value >> 8));
+		*(std::uint16_t *)(memory + vaddr) = value;
 	}
 
 	void MemoryWrite32(std::uint64_t vaddr, std::uint32_t value) override {
-		MemoryWrite16(vaddr, std::uint16_t(value));
-		MemoryWrite16(vaddr + 2, std::uint16_t(value >> 16));
+		*(std::uint32_t *)(memory + vaddr) = value;
 	}
 
 	void MemoryWrite64(std::uint64_t vaddr, std::uint64_t value) override {
-		MemoryWrite32(vaddr, std::uint32_t(value));
-		MemoryWrite32(vaddr + 4, std::uint32_t(value >> 32));
+		*(std::uint64_t *)(memory + vaddr) = value;
 	}
 	
 	void MemoryWrite128(std::uint64_t vaddr, Dynarmic::A64::Vector value) override {
-		MemoryWrite64(vaddr, value[0]);
-		MemoryWrite64(vaddr + 8, value[1]);
+		*(std::uint64_t *)(memory + vaddr) = value[0];
+		*(std::uint64_t *)(memory + vaddr + 8) = value[1];
+	}
+	
+	bool MemoryWriteExclusive8(std::uint64_t vaddr, std::uint8_t value, [[maybe_unused]] std::uint8_t expected) override {
+        MemoryWrite8(vaddr, value);
+        return true;
+    }
+    bool MemoryWriteExclusive16(std::uint64_t vaddr, std::uint16_t value, [[maybe_unused]] std::uint16_t expected) override {
+        MemoryWrite16(vaddr, value);
+        return true;
+    }
+    bool MemoryWriteExclusive32(std::uint64_t vaddr, std::uint32_t value, [[maybe_unused]] std::uint32_t expected) override {
+        MemoryWrite32(vaddr, value);
+        return true;
+    }
+    bool MemoryWriteExclusive64(std::uint64_t vaddr, std::uint64_t value, [[maybe_unused]] std::uint64_t expected) override {
+        MemoryWrite64(vaddr, value);
+        return true;
+    }
+    bool MemoryWriteExclusive128(std::uint64_t vaddr, Dynarmic::A64::Vector value, [[maybe_unused]] Dynarmic::A64::Vector expected) override {
+        MemoryWrite128(vaddr, value);
+        return true;
     }
 
 	void InterpreterFallback(std::uint64_t pc, size_t num_instructions) override {
-		// This is never called in practice.
-		std::terminate();
+		printf("Interpreter fallback: 0x%llx\n", pc);
 	}
 
 	void CallSVC(std::uint32_t swi) override {
-		// Do something.
+		printf("CallSVC 0x%x\n", swi);
 	}
 
 	void ExceptionRaised(std::uint64_t pc, Dynarmic::A64::Exception exception) override {
-		// Do something.
+		printf("Exception raised: 0x%llx\n", pc);
 	}
 
 	void AddTicks(std::uint64_t ticks) override {
-		total_ticks += ticks;
-		
 		if (ticks > ticks_left) {
 			ticks_left = 0;
 			return;
@@ -103,8 +114,8 @@ public:
 	}
 	
 	std::uint64_t GetCNTPCT() override {
-        return total_ticks;
-    }
+		return 0x10000000000 - ticks_left;
+	}
 };
 
 extern so_env so_dynarec_env;
